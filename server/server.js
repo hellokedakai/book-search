50a077d9fa9c43e2c331cd0ad3444b698d707cb5
Kedakai <nicoleteeth@ucla.edu>
2021-08-08 15:57:30 -0700

    change port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 // create a new Apollo server and pass in our schema data
