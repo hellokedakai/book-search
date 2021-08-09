144ec75dd8473c1368d9046c9e3d9a37591e638d
Kedakai <nicoleteeth@ucla.edu>
2021-08-08 18:41:02 -0700

    install and set up apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3002/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+    {/* <Router> */}
       <>
         <Navbar />
         <Switch>
@@ -15,7 +26,8 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+    {/* </Router> */}
+    </ApolloProvider>
   );
 }
 
