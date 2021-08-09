09d3c31a5dd47fa2489eee6c8aaf756abaa89446
Kedakai <nicoleteeth@ucla.edu>
2021-08-08 21:15:10 -0700

    add savebook mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,10 +25,18 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook()
+  mutation saveBook($authors: [String], $description: String, $bookId: String, $image: String, $link: String, $title: String) {
+      saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+          
+      }
+
+  }
 
 `;
 
 export const REMOVE_BOOK = gql`
+  mutation removeBook() {
+
+  }
 
 `;
